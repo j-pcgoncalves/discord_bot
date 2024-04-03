ede712e746bf2cf6377d2e0282eeb78942f8ecfb
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-04-03 22:05:53 +0100

    register slash commands

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -1,5 +1,30 @@
-function clientReadyHandler(client) {
+const { 
+    REST, 
+    Routes,
+} = require("discord.js");
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+async function clientReadyHandler(client) {
     console.log(`Logged In as ${client.user.tag}!`);
+
+    try {
+        console.log(`Started refreshing ${client.commands.size} command(s)!`);
+    
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            {
+                body: client.commands.map((command) => {
+                    return command.data.toJSON();
+                }),
+            }
+        );
+    
+        console.log(`Successfully reloaded ${data.length} command(s)!`);
+    } catch (error) {
+        console.error(error);
+    }
+
 };
 
 module.exports = {
