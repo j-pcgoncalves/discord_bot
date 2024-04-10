43c35d7d4182086adad11d23e4ec784ba379c2c9
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-04-10 21:35:41 +0100

    create and register the forecast command

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const {
 const { clientReadyHandler } = require("./events/clientReady");
 const { interactionCreateHandler } = require("./events/interactionCreate");
 const pingCommand = require("./commands/ping");
+const forecastCommand = require("./commands/forecast");
 
 const client = new Client({
     intents: [
@@ -20,6 +21,7 @@ const client = new Client({
 client.commands = new Collection();
 
 client.commands.set(pingCommand.data.name, pingCommand);
+client.commands.set(forecastCommand.data.name, forecastCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
 client.on(Events.InteractionCreate, interactionCreateHandler);
