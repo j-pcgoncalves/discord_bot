2712a3865c57dc24276552cb88ff01abff03756b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-04-10 23:30:41 +0100

    create and register astro command

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ const {
 
 const { clientReadyHandler } = require("./events/clientReady");
 const { interactionCreateHandler } = require("./events/interactionCreate");
+
 const pingCommand = require("./commands/ping");
 const forecastCommand = require("./commands/forecast");
+const astroCommand = require("./commands/astro");
 
 const client = new Client({
     intents: [
@@ -22,6 +24,7 @@ client.commands = new Collection();
 
 client.commands.set(pingCommand.data.name, pingCommand);
 client.commands.set(forecastCommand.data.name, forecastCommand);
+client.commands.set(astroCommand.data.name, astroCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
 client.on(Events.InteractionCreate, interactionCreateHandler);
